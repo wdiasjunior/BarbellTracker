9e3f65899d29d9eaf58e48e0a5987a296fc35bd4
Wellington Junior <wdiasjunior@gmail.com>
2021-08-02 23:18:13 -0300

    Initial commit

---
+++ binaryMask.js
@@ -0,0 +1,325 @@
+(function localFileVideoPlayer() {
+  'use strict'
+  var playSelectedFile = function(event) {
+    var file = this.files[0]
+    var URL = window.URL || window.webkitURL
+    var fileURL = URL.createObjectURL(file)
+    var videoNode = document.querySelector('video')
+    videoNode.src = fileURL
+  }
+  var inputNode = document.querySelector('input')
+  inputNode.addEventListener('change', playSelectedFile, true)
+
+  // document.getElementById('deviceCameraButton').disabled = false;
+
+  // setTimeout(processVideo, 0); // ??? auto process video after is loaded?
+  // delete all and reinitiale variables (matrixes, contours and lines)
+})()
+
+function loadSampleVideo() {
+  document.getElementById("videoInput").removeAttribute("src");
+  document.getElementById("videoInput").setAttribute("src", "media/2.mp4");
+  // document.getElementById("videoInput").load();
+  // document.getElementById("videoInput").play();
+  document.getElementById("videoInput").pause();
+}
+
+let streaming = false;
+function playPause() {
+  if(streaming == false) {
+    videoInput.play();
+    document.getElementById('playPause').innerHTML = "Stop";
+  } else {
+    videoInput.pause();
+    document.getElementById('playPause').innerHTML = "Play";
+  }
+  streaming = !streaming;
+}
+
+function onOpenCvReady() {
+  console.log('OpenCV Ready');
+  document.getElementById('status').innerHTML = "OpenCV is Ready";
+  document.getElementById('videoInputButton').disabled = false;
+}
+
+function setup() {
+  // TODO initialize variables
+
+  // put processVideo() here ?
+
+  // setTimeout(processVideo, 0);
+}
+
+function downloadVideo() {
+  // TODO
+  // convert canvas to webm and then convert to mp4
+}
+
+var cameraInput = false;
+
+function changeInputSource(n) {
+  // TODO switch between device camera and file input
+
+  // document.getElementById('deviceCameraButton').disabled = true;
+
+  if(n == 0) {
+    cameraInput = false;
+    // TODO
+  } else if(n == 1) {
+    cameraInput = true;
+    // TODO
+  }
+
+}
+
+let weightUnit = 'kg';
+let weight;
+function weightUnitChanged(n) {
+  weight = document.getElementById('weight').value;
+
+  if(n == 1) {
+    document.getElementById('lbs').disabled = true;
+    document.getElementById('kg').disabled = false;
+    weightUnit = 'lbs';
+    weight = weight * 2.2;
+    weight = weight.toFixed(2);
+    document.getElementById('weight').value = weight;
+  } else {
+    document.getElementById('lbs').disabled = false;
+    document.getElementById('kg').disabled = true;
+    weightUnit = 'kg';
+    weight = weight / 2.2;
+    weight = weight.toFixed(2);
+    document.getElementById('weight').value = weight;
+  }
+}
+
+let isConcentric = false;
+let rest = true;
+let yVelocity = 0;
+// let velocity = 0;
+let velocities = [];
+let avgVelocity = 0;
+let peakVelocity = 0;
+let acceleration = 0;
+let power = 0;
+let hDisplacement = 0;
+let lastY = 0;
+let lastX = 0;
+let xDisp = 0;
+let yDisp = 0;
+// let xDistance = 0;
+let yDistance = 0;
+// let distance = 0;
+let barRadius = 25;
+let mmpp = 0;
+let initialY = 0;
+let oscilation = 40;
+let radius = null;
+let refRadius = null;
+
+function barMath(x, y) {
+
+  // acceleration (m/s^2)
+  // speed/velocity (concentric only m/s)
+  // power (kW)
+  // horizontal displacement (cm)
+  // current, average, min, max(peak) of each above ?
+
+  if(initialY == 0) {
+    initialY = y;
+  }
+  if((initialY + oscilation) < y) { // checks for rep. ignores bar whip in between reps
+    rest = false;
+    if(y < lastY) { // checks if movement is concentric
+      isConcentric = true;
+      // velocity = 0;
+      yVelocity = 0;
+      if (radius / height > 0.0125) {
+        if(refRadius == null) {
+          refRadius = radius;
+          mmpp = barRadius / refRadius;
+        }
+        xDisp = lastX - x;
+        xDisp = (xDisp / 10);
+        yDisp = lastY - y
+        // xDistance = xDisp * mmpp;
+        yDistance = yDisp * mmpp;
+        // distance = Math.sqrt(xDisp ** 2 + yDisp ** 2) * mmpp;
+        if(Math.abs(yDistance) > (barRadius / 4)) {
+          // velocity = distance * FPS / 1000;
+          yVelocity = yDistance * FPS / 1000;
+          velocities.push(yVelocity);
+        }
+      }
+    } else {
+      isConcentric = false;
+      lastY = y;
+      lastX = x;
+    }
+  } else {
+    lastY = 0;
+    lastX = 0;
+    rest = true;
+  }
+
+  let sum = 0;
+  for(let i = 0; i < velocities.length; i++) {
+    sum += velocities[i];
+  }
+  avgVelocity = (sum / velocities.length) / 10;
+  acceleration = (avgVelocity / (FPS / 1000)) / 10;
+
+  document.getElementById("velocity").innerHTML = avgVelocity.toFixed(2);
+  document.getElementById("acceleration").innerHTML = acceleration.toFixed(2);
+  // document.getElementById("power").innerHTML = power;
+  document.getElementById("horizontalDisplacement").innerHTML = -xDisp.toFixed(2);
+
+}
+
+let red = 0;
+let green = 255;
+let blue = 0;
+
+function pathColorChange(color) {
+  if(color == 'red') {
+    red = 255;
+    green = 0;
+    blue = 0;
+    document.getElementById('redButton').disabled = true;deviceCameraButton
+    document.getElementById('greenButton').disabled = false;
+    document.getElementById('blueButton').disabled = false;
+  } else if(color == 'green') {
+    red = 0;
+    green = 255;
+    blue = 0;
+    document.getElementById('redButton').disabled = false;
+    document.getElementById('greenButton').disabled = true;
+    document.getElementById('blueButton').disabled = false;
+  } else if(color == 'blue') {
+    red = 0;
+    green = 0;
+    blue = 255;
+    document.getElementById('redButton').disabled = false;
+    document.getElementById('greenButton').disabled = false;
+    document.getElementById('blueButton').disabled = true;
+  }
+}
+
+const width = 432;
+const height = 768;
+const FPS = 30;
+
+let count = 0;
+let centerPointArray = new Array();
+
+function processVideo() {
+
+  const videoInput = document.getElementById("videoInput");
+  const canvas = document.getElementById("canvasOutput");
+  let src = new cv.Mat(height, width, cv.CV_8UC4);
+  let dst = new cv.Mat(height, width, cv.CV_8UC1);
+  let cap = new cv.VideoCapture(videoInput);
+  document.getElementById("playPause").style = display = true;
+
+  let begin = Date.now();
+  cap.read(src);
+
+  let hsv = new cv.Mat();
+  cv.cvtColor(src, hsv, cv.COLOR_RGBA2RGB);
+  cv.cvtColor(hsv, hsv, cv.COLOR_RGB2HSV);
+
+  let greenLower = new cv.Scalar(50, 130, 97);
+  let greenUpper = new cv.Scalar(64, 255, 255);
+
+  // let greenLower = new cv.Scalar(33, 46, 80);
+  // let greenUpper = new cv.Scalar(86, 156, 255);
+
+  // let greenLower = new cv.Scalar(29, 86, 6);
+  // let greenUpper = new cv.Scalar(143, 255, 86);
+
+  // let greenLower = new cv.Scalar(95, 110, 71);
+  // let greenUpper = new cv.Scalar(79, 255, 67);
+
+  let low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), greenLower);
+  let high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), greenUpper);
+
+  let mask = new cv.Mat();
+  let mask1 = new cv.Mat();
+  let mask2 = new cv.Mat();
+  cv.inRange(hsv, low, high, mask1);
+  cv.threshold(mask1, mask, 120, 255, cv.THRESH_BINARY);
+  cv.bitwise_not(mask, mask2);
+
+  let M = cv.Mat.ones(5, 5, cv.CV_8U);
+  let anchor = new cv.Point(-1, -1);
+  cv.erode(mask1, mask, M, anchor, 2, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+  cv.dilate(mask1, mask, M, anchor, 2, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+
+  let contours = new cv.MatVector();
+  let hierarchy = new cv.Mat();
+  let cnts = contours.get(0);
+
+  cv.findContours(mask, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+
+  if(contours.size() == 0) {
+    console.log('Could not recognize tracking point.')
+    alert('Could not recognize tracking point.')
+  }
+
+  // for(let i = 0; i < contours.size(); i++) {
+  //   cv.drawContours(src, contours, i, [0, 255, 0, 255], 2, cv.LINE_8, hierarchy, 100);
+  // }
+
+  let Moments;
+  let M00;
+  let M10;
+  let M00Array = [0,];
+
+  for(let j = 0; j < contours.size(); j++) {
+    cnts = contours.get(j);
+    Moments = cv.moments(cnts,false);
+    M00Array[j] = Moments.m00;
+  }
+
+  Moments = cv.moments(cnts, false);
+  M00 = Moments.m00;
+  M10 = Moments.m10;
+  M01 = Moments.m01;
+  x_cm = M10/M00;
+  y_cm = M01/M00;
+
+  radius = cv.minEnclosingCircle(cnts).radius;
+  barMath(x_cm, y_cm);
+
+  let centerPoint = new cv.Point(x_cm, y_cm);
+  centerPointArray[count] = centerPoint;
+  count++;
+  cv.circle(src, centerPoint, 4, [0, 0, 255, 255], 2, cv.LINE_AA, 0);
+
+  for(let k = 0; k < centerPointArray.length; k++) {
+    // console.log(centerPointArray[k - 1])
+    if(centerPointArray[k - 1] == null || centerPointArray[k] == null){
+      continue;
+    }
+    cv.line(src, centerPointArray[k - 1], centerPointArray[k], [red, green, blue, 255], 2); // pathColor [0, 255, 0, 255]
+  }
+
+  cv.imshow("canvasOutput", src);
+
+  dst.delete();
+  src.delete();
+  mask.delete();
+  mask1.delete();
+  mask2.delete();
+  low.delete();
+  high.delete();
+  contours.delete();
+  hierarchy.delete();
+  hsv.delete();
+
+  let delay = 1000/FPS - (Date.now() - begin);
+  setTimeout(processVideo, delay);
+}
+
+// setTimeout(processVideo, 0);
---
+++ camshift.js
@@ -0,0 +1,225 @@
+(function localFileVideoPlayer() {
+  'use strict'
+  var playSelectedFile = function(event) {
+    var file = this.files[0]
+    var URL = window.URL || window.webkitURL
+    var fileURL = URL.createObjectURL(file)
+    var videoNode = document.querySelector('video')
+    videoNode.src = fileURL
+  }
+  var inputNode = document.querySelector('input')
+  inputNode.addEventListener('change', playSelectedFile, false)
+
+  // document.getElementById('deviceCameraButton').disabled = false;
+
+  // setTimeout(processVideo, 0); // ??? auto process video after is loaded?
+  // delete all and reinitiale variables (matrixes, contours and lines)
+})()
+
+function loadSampleVideo() {
+  document.getElementById("videoInput").removeAttribute("src");
+  document.getElementById("videoInput").setAttribute("src", "media/2.mp4");
+  // document.getElementById("videoInput").load();
+  document.getElementById("videoInput").pause();
+}
+
+function onOpenCvReady() {
+  console.log('OpenCV Ready');
+  document.getElementById('status').innerHTML = "OpenCV is Ready";
+  document.getElementById('videoInputButton').disabled = false;
+}
+
+let width, height;
+function setSize() {
+  // if (window.orientation == 0) {
+    width = 432; height = 768; //portrait
+  // }
+  // else {
+  //   width = 768; height = 432; //landscape
+  // }
+}
+setSize();
+const constraints = {
+  video: { facingMode: "environment", }, audio: false
+};
+const video = document.getElementById("videoInput");
+const canvas = document.getElementById('output');
+const canvasPos = canvas.getBoundingClientRect();
+canvas.width = width; canvas.height = height;
+canvas.addEventListener('mousedown', down);
+canvas.addEventListener('mouseup', up);
+canvas.addEventListener('mousemove', move);
+canvas.addEventListener('mouseleave', up);
+canvas.addEventListener('touchstart', down);
+canvas.addEventListener('touchend', up);
+canvas.addEventListener('touchmove', move);
+
+function successCallback(stream) {
+  video.width = width; video.height = height;//prevent Opencv.js error.
+  canvas.width = width; canvas.height = height;
+  video.srcObject = stream;
+  video.play();
+  setTimeout(setupCV, 0);
+  // streaming = !streaming;
+}
+
+let streaming = false;
+function run() {
+  const stream = video.srcObject;
+  if(streaming == false) {
+    video.width = width; video.height = height;//prevent Opencv.js error.
+    canvas.width = width; canvas.height = height;
+    video.srcObject = stream;
+    video.play();
+    setTimeout(setupCV, 0);
+    document.getElementById('toggleStream').innerHTML = "Stop";
+  } else {
+    // const stream = video.srcObject;
+    // const tracks = stream.getTracks();
+    // tracks.forEach(track => {
+    //   track.stop();
+    // });
+    video.pause();
+    document.getElementById('toggleStream').innerHTML = "Play";
+  }
+  streaming = !streaming;
+}
+
+// function errorCallback(error) {
+//   console.log(error);
+// }
+//
+// let streaming = false;
+function toggleStream() {
+  if (streaming === false) {
+    // navigator.getUserMedia(constraints, successCallback, errorCallback);
+    successCallback();
+    document.getElementById('toggleStream').innerHTML = "Stop";
+  }
+  else {
+    // const stream = video.srcObject;
+    // const tracks = stream.getTracks();
+    // tracks.forEach(track => {
+    //   track.stop();
+    // });
+    video.pause();
+    document.getElementById('toggleStream').innerHTML = "Play";
+  }
+  streaming = !streaming;
+}
+
+let drawing = false;
+let startX, startY, moveX, moveY;
+function down(evt) {
+    try {
+        startX = Math.round(evt.touches[0].clientX - canvasPos.left);
+        startY = Math.round(evt.touches[0].clientY - canvasPos.top);
+    } catch{
+        startX = Math.round(evt.clientX - canvasPos.left);
+        startY = Math.round(evt.clientY - canvasPos.top);
+    }
+    moveX = startX; moveY = startY;
+    drawing = true;
+    tracking = false;
+    inProcess = false;
+}
+
+let tracking = false;
+function up(evt) {
+    drawing = false;
+    tracking = true;
+}
+
+function move(evt) {
+    if (drawing == true) {
+        try {
+            moveX = Math.round(evt.touches[0].clientX - canvasPos.left);
+            moveY = Math.round(evt.touches[0].clientY - canvasPos.top);
+        } catch{
+            moveX = Math.round(evt.clientX - canvasPos.left);
+            moveY = Math.round(evt.clientY - canvasPos.top);
+        }
+    }
+}
+
+let frame, cap;
+async function setupCV() {
+    if (frame == undefined) {
+        cap = await new cv.VideoCapture('videoInput');
+        frame = await new cv.Mat(height, width, cv.CV_8UC4);
+        console.log("cv setup complete.");
+    }
+    setTimeout(process, 0);
+}
+
+function process() {
+    if (streaming === true) {
+        cap.read(frame);
+        if (drawing === true) {
+            cv.rectangle(frame, new cv.Point(startX, startY), new cv.Point(moveX, moveY), new cv.Scalar(255, 0, 0, 255), 2);
+        }
+        if (tracking === true) {
+            try {
+                camShift();
+            }
+            catch {
+                console.log("Error on camshift.");
+                tracking = false;
+            }
+        }
+        cv.imshow('output', frame);
+        setTimeout(process, 33);
+    }
+}
+
+let inProcess = false;
+let trackWindow, roi, hsvRoi, mask, lowScalar, highScalar, low, high, roiHist, hsvRoiVec, termCrit, hsv, dst, hsvVec, trackBox;
+function camShift() {
+    if (inProcess === false) {
+        console.log('Setup camshift.');
+        inProcess = true;
+        const p1 = Math.min(startX, moveX);
+        const p2 = Math.min(startY, moveY);
+        const p3 = Math.max(startX, moveX) - p1;
+        const p4 = Math.max(startY, moveY) - p2;
+        trackWindow = new cv.Rect(p1, p2, p3, p4);
+        roi = frame.roi(trackWindow);
+        hsvRoi = new cv.Mat();
+        cv.cvtColor(roi, hsvRoi, cv.COLOR_RGBA2RGB);
+        cv.cvtColor(hsvRoi, hsvRoi, cv.COLOR_RGB2HSV);
+        mask = new cv.Mat();
+        lowScalar = new cv.Scalar(30, 30, 0);
+        highScalar = new cv.Scalar(180, 180, 180);
+        low = new cv.Mat(hsvRoi.rows, hsvRoi.cols, hsvRoi.type(), lowScalar);
+        high = new cv.Mat(hsvRoi.rows, hsvRoi.cols, hsvRoi.type(), highScalar);
+        cv.inRange(hsvRoi, low, high, mask);
+        roiHist = new cv.Mat();
+        hsvRoiVec = new cv.MatVector();
+        hsvRoiVec.push_back(hsvRoi);
+        cv.calcHist(hsvRoiVec, [0], mask, roiHist, [180], [0, 180]);
+        cv.normalize(roiHist, roiHist, 0, 255, cv.NORM_MINMAX);
+
+        roi.delete(); hsvRoi.delete(); mask.delete(); low.delete(); high.delete(); hsvRoiVec.delete();
+        termCrit = new cv.TermCriteria(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1);
+
+        hsv = new cv.Mat(height, width, cv.CV_8UC3);
+        dst = new cv.Mat();
+        hsvVec = new cv.MatVector();
+        hsvVec.push_back(hsv);
+        trackBox = null;
+    }
+
+    cap.read(frame);
+    cv.cvtColor(frame, hsv, cv.COLOR_RGBA2RGB);
+    cv.cvtColor(hsv, hsv, cv.COLOR_RGB2HSV);
+    cv.calcBackProject(hsvVec, [0], roiHist, dst, [0, 180], 1);
+
+    [trackBox, trackWindow] = cv.CamShift(dst, trackWindow, termCrit);
+
+    // Draw it on image
+    let pts = cv.rotatedRectPoints(trackBox);
+    cv.line(frame, pts[0], pts[1], [255, 0, 0, 255], 3);
+    cv.line(frame, pts[1], pts[2], [255, 0, 0, 255], 3);
+    cv.line(frame, pts[2], pts[3], [255, 0, 0, 255], 3);
+    cv.line(frame, pts[3], pts[0], [255, 0, 0, 255], 3);
+}
